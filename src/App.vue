892738481737ba0f36c5a98d341ffdb1df128c55
Vincent Largillet <lrgl.vincent@gmail.com>
2022-02-03 17:17:17 +0100

    Remove HelloWorld and change content of app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main class="container">
+    <h1>Running TensorFlowJS in version : {{ version }}</h1>
+    <div class="predict-container"></div>
+  </main>
 </template>
+
+<script setup>
+import * as tf from '@tensorflow/tfjs'
+import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm'
+import * as tfjsCpu from '@tensorflow/tfjs-backend-cpu'
+import * as tfjsWebGL from '@tensorflow/tfjs-backend-webgl'
+
+tfjsWasm.setWasmPaths(
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/`
+)
+tfjsCpu.setCpuPaths(
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsCpu.version_cpu}/dist/`
+)
+tfjsWebGL.setCpuPaths(
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWebGL.version_webgl}/dist/`
+)
+
+import { onMounted, ref } from '@vue/runtime-core'
+
+const version = ref(tf.version_core)
+
+const model = ref()
+const matrix = ref([])
+
+const getPredictions = async () => {
+  //const returnTensor = false
+  //const predictions = await model.predict(matrix, returnTensor)
+
+  //If predictions isn't null, do something
+  if (predictions.length > 0) {
+    //Do something for each predictions in the array
+    for (let i = 0; i < predictions.length; i++) {}
+  }
+}
+
+onMounted(async () => {
+  await tf.ready()
+})
+</script>
 
 <style>
 #app {
